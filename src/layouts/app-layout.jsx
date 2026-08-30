7177b0fe9799195023da42e4245d038b35759abe
Aadesh Vishwakarma <aadeshvishwakarma52@gmail.com>
2026-08-30 19:59:33 +0530

    done

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,5 +1,5 @@
 import Header from "@/components/header";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -9,7 +9,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by RoadsideCoder
+        Made with 💗 by Aadesh
       </div>
     </div>
   );
